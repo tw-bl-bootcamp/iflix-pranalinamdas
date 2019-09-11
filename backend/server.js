d9e09815f811fa627a6885f277127129e8a5ff7d
pranalikisannamdas <pranalinamdas96@gmail.com>
2019-09-11 12:26:35 +0530

    [Pranali] Add. mongoose to server for mongodb database connection

--- backend/server.js
+++ backend/server.js
@@ -11,3 +11,25 @@ app.use(expressValidator())
 app.listen(3000, () => {
     console.log("Server connected to port 3000")
 })
+
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+const dbConfig = require('./Config/dbConfig.js')
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Successfully connected to  database")
+}).catch((error) => {
+    console.log("Could not connect to database")
+    process.exit()
+})
+
+mongoose.connection.on('error', function (err) {
+    console.log("mongoose default connection has occured" + err + "error")
+})
+
+mongoose.connection.on('disconnected', function () {
+    console.log(disconnected("mongoose default connection is disconnected"))
+})
