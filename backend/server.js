fe6534755aa3ef7221ff21232351094391382d1b
pranalikisannamdas <pranalinamdas96@gmail.com>
2019-09-11 18:09:45 +0530

    [Pranali] Add. get for listening requests

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const express = require('express')
-
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
 
+app.get('/',(req, res) => {
+    res.send({message:'successfull'})
+})
 module.exports = app
--- backend/test/ServerTest.js
+++ backend/test/ServerTest.js
@@ -2,9 +2,19 @@ const chai = require('chai')
 const expect = chai.expect
 chai.should()
 const app = require('../server')
+const http = require('chai-http')
+chai.use(http)
 
 describe('Server App', ()=>{
     it('should present in the server', ()=>{
         expect(app).to.be.a('function')
+    })
+
+    it('should send response 200', (done)=>{
+        chai.request(app).get('/')
+        .then((res)=>{
+            express(res).to.have.status(200)
+        })
+        done()
     })
 })
